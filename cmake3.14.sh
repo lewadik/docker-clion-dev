9fbe0671ef5e180de4045a75de90cde789c72576
lewadik <lewadik@gmail.com>
2019-04-28 22:38:30 +0300

    Update cmake3.14.sh

--- cmake3.14.sh
+++ cmake3.14.sh
@@ -5,4 +5,4 @@ cd ~/temp
 wget https://cmake.org/files/v$version/cmake-$version.$build-Linux-x86_64.sh 
 mkdir /opt/cmake
 sh cmake-$version.$build-Linux-x86_64.sh --prefix=/opt/cmake
-ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
+ln -s /opt/cmake/bin/cmake /usr/bin/cmake
